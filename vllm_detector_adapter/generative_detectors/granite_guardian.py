3350fe42284a7314b6f28a3e66f583021bb21f64
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2025-01-29 14:48:20 -0700

    :pencil2: Update comments
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- vllm_detector_adapter/generative_detectors/granite_guardian.py
+++ vllm_detector_adapter/generative_detectors/granite_guardian.py
@@ -94,12 +94,12 @@ class GraniteGuardian(ChatCompletionDetectionBase):
             )
 
         if len(request.context) > 1:
-            # The API supports more than one context text but currently chat completions
-            # will only take one context
+            # The detector API for context docs detection supports more than one context text
+            # but currently chat completions will only take one context.
             logger.warning("More than one context provided. Only the last will be used")
         context_text = request.context[-1]
         content = request.content
-        # The "context" role is not an officially support OpenAI role, so this is specific
+        # The "context" role is not an officially supported OpenAI role, so this is specific
         # to Granite Guardian. Messages must also be in precise ordering, or model/template
         # errors may occur.
         if risk_name in self.RESPONSE_CONTEXT_ANALYSIS_RISKS:
