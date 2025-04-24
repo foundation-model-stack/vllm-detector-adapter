6a34f9c1a61ee6ecb42a249f47fc001810196894
Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
2025-04-24 12:26:24 -0600

    :arrow_up: Update base image and vllm compatibility to 0.8.3-0.8.4 (#51)
    * :arrow_up: Bump to latest vllm and vllm-tgis-adapter releases
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :construction_worker: Update base
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :alien: Update import for vllm 0.8.3
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :pushpin: Allow RCs
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :arrow_up: Expand version range
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :bug: Update tag
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :arrow_up: Bump vllm base
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    * :recycle: Backwards compatibility
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Evaline Ju <69598118+evaline-ju@users.noreply.github.com>

--- vllm_detector_adapter/api_server.py
+++ vllm_detector_adapter/api_server.py
@@ -16,7 +16,6 @@ from vllm.entrypoints.logger import RequestLogger
 from vllm.entrypoints.openai import api_server
 from vllm.entrypoints.openai.cli_args import make_arg_parser, validate_parsed_serve_args
 from vllm.entrypoints.openai.protocol import ErrorResponse
-from vllm.entrypoints.openai.reasoning_parsers import ReasoningParserManager
 from vllm.entrypoints.openai.serving_models import BaseModelPath, OpenAIServingModels
 from vllm.entrypoints.openai.tool_parsers import ToolParserManager
 from vllm.utils import FlexibleArgumentParser, is_valid_ipv6_address, set_ulimit
@@ -35,6 +34,13 @@ from vllm_detector_adapter.protocol import (
     GenerationDetectionRequest,
 )
 from vllm_detector_adapter.utils import LocalEnvVarArgumentParser
+
+try:
+    # Third Party
+    from vllm.entrypoints.openai.reasoning_parsers import ReasoningParserManager
+except ImportError:
+    # Third Party
+    from vllm.reasoning import ReasoningParserManager
 
 TIMEOUT_KEEP_ALIVE = 5  # seconds
 
