506f12ba35fe4e05c99db7129bae37b9c86d6db8
Paulo Caldeira <7291154+pmcjr@users.noreply.github.com>
2025-05-12 13:17:50 -0300

    Add skipping/short circuit to workflow if already on latest version
    Signed-off-by: Paulo Caldeira <7291154+pmcjr@users.noreply.github.com>

--- scripts/update_vllm_version.py
+++ scripts/update_vllm_version.py
@@ -1,6 +1,7 @@
 # Standard
 import argparse
-import json
+import os
+import sys
 import tomllib
 
 # Third Party
@@ -21,6 +22,11 @@ def parse_args():
     return parser.parse_args()
 
 
+def parse_current_version(vllm_string):
+    version = vllm_string.split("vllm.git@v", 1)[1].split(" ;")[0]
+    return version
+
+
 def main():
 
     args = parse_args()
@@ -29,17 +35,28 @@ def main():
 
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
-        data["project"]["optional-dependencies"]["vllm"][
-            0
-        ] = f"vllm @ git+https://github.com/vllm-project/vllm.git@v{args.vllm_version} ; sys_platform == 'darwin'"
-        data["project"]["optional-dependencies"]["vllm"][
-            1
-        ] = f"vllm=={args.vllm_version} ; sys_platform != 'darwin'"
+        current_version = parse_current_version(
+            vllm_string=data["project"]["optional-dependencies"]["vllm"][0]
+        )
+        if current_version == args.vllm_version:
+            print(
+                f"VLLM already updated to latest version, skipping update and setting 'SKIP_VERSION' output!"
+            )
+            with open(os.getenv("GITHUB_OUTPUT"), "a") as env:
+                print(f"SKIP_VERSION=true", file=env)
+            sys.exit(0)
+        else:
+            data["project"]["optional-dependencies"]["vllm"][
+                0
+            ] = f"vllm @ git+https://github.com/vllm-project/vllm.git@v{args.vllm_version} ; sys_platform == 'darwin'"
+            data["project"]["optional-dependencies"]["vllm"][
+                1
+            ] = f"vllm=={args.vllm_version} ; sys_platform != 'darwin'"
 
-    with open("pyproject.toml", "wb") as f:
-        tomli_w.dump(data, f)
+            with open("pyproject.toml", "wb") as f:
+                tomli_w.dump(data, f)
 
-    print(f"VLLM version updated to {args.vllm_version} successfully!")
+            print(f"VLLM version updated to {args.vllm_version} successfully!")
 
 
 if __name__ == "__main__":
